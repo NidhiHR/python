bf6fc353c35b0360e252cc94bc525c1184b5293e
NidhiHR <165025461+NidhiHR@users.noreply.github.com>
2024-03-27 17:15:40 +0530

    Update main.py

--- Projects/Quizgame/main.py
+++ Projects/Quizgame/main.py
@@ -1,45 +1,4 @@
 
-# from data import question_data
-# Score=0
-
-# question_bank=[]
-
-# class Quiz:
-#     def __init__(self,question,answer):
-#         self.question=question
-#         self.answer=answer
-
-# class Question:
-#     def __init__(self,q_list) :
-#         self.question_number=0
-#         self.question_list=q_list
-
-#     def next_question(self):
-#         while self.question_number<len(self.question_list):
-#             cur_question=self.question_list[self.question_number]
-#             ip=input(f"{self.question_number+1} : {cur_question.question} (True/False): ")
-#             if ip.lower()==cur_question.answer.lower():
-#                 global Score
-#                 Score+=1
-#                 print(f"YOUR ANSWER IS CORRECT AND YOUR SCORE IS {Score}")
-#             else:
-#                 print(f"YOUR ANSWER IS Wrong AND YOUR SCORE IS {Score}")
-#             self.question_number+=1
-#         else:
-#             print("END OF QUIZ")
-
-# for question in question_data:
-#     question_text=question['text']
-#     question_answer=question["answer"]
-#     new_question=Quiz(question_text,question_answer)
-#     question_bank.append(new_question)
-
-# quiz=Question(question_bank)
-# quiz.next_question()
-
-
-
-
 from data import question_data
 
 question_bank=[]
@@ -87,5 +46,48 @@ while quiz.still_has_question():
 else:
     print("\n\n ITS THE END OF THE GAME")
     print(f"UR SCORE IS {quiz.score}/{len(quiz.question_list)}")
+
+
+# from data import question_data
+# Score=0
+
+# question_bank=[]
+
+# class Quiz:
+#     def __init__(self,question,answer):
+#         self.question=question
+#         self.answer=answer
+
+# class Question:
+#     def __init__(self,q_list) :
+#         self.question_number=0
+#         self.question_list=q_list
+
+#     def next_question(self):
+#         while self.question_number<len(self.question_list):
+#             cur_question=self.question_list[self.question_number]
+#             ip=input(f"{self.question_number+1} : {cur_question.question} (True/False): ")
+#             if ip.lower()==cur_question.answer.lower():
+#                 global Score
+#                 Score+=1
+#                 print(f"YOUR ANSWER IS CORRECT AND YOUR SCORE IS {Score}")
+#             else:
+#                 print(f"YOUR ANSWER IS Wrong AND YOUR SCORE IS {Score}")
+#             self.question_number+=1
+#         else:
+#             print("END OF QUIZ")
+
+# for question in question_data:
+#     question_text=question['text']
+#     question_answer=question["answer"]
+#     new_question=Quiz(question_text,question_answer)
+#     question_bank.append(new_question)
+
+# quiz=Question(question_bank)
+# quiz.next_question()
+
+
+
+
 
 
